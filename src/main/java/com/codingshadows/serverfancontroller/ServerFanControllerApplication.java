2a7d7b45b05d90ed70d4c2c7f8f9b2bca53d0927
Rusu Dinu Stefan <dinu@codingshadows.com>
2021-01-29 09:52:10 +0200

    Initial commit

---
+++ src/main/java/com/codingshadows/serverfancontroller/ServerFanControllerApplication.java
@@ -0,0 +1,13 @@
+package com.codingshadows.serverfancontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerFanControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerFanControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingshadows/serverfancontroller/ServerFanControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingshadows.serverfancontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerFanControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
